c437570e252ca2f34270117a27b1849cc0573a39
Mohin <122416157+Mohin2295747@users.noreply.github.com>
2025-06-14 10:31:14 +0600

    Update Filters.kt

--- src/zh/hanime1/src/eu/kanade/tachiyomi/animeextension/zh/hanime1/Filters.kt
+++ src/zh/hanime1/src/eu/kanade/tachiyomi/animeextension/zh/hanime1/Filters.kt
@@ -2,119 +2,48 @@ package eu.kanade.tachiyomi.animeextension.zh.hanime1
 
 import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 
-class QueryFilter(
-    name: String,
-    val key: String,
-    values: Array<String>,
-) : AnimeFilter.Select<String>(name, values) {
+open class QueryFilter(name: String, val key: String, values: Array<String>) :
+    AnimeFilter.Select<String>(name, values) {
     val selected: String
-        get() = if (state == 0) "" else values[state]
+        get() = if (state == 0) {
+            ""
+        } else {
+            values[state]
+        }
 }
 
-class TagFilter(
-    val key: String,
-    name: String,
-    state: Boolean = false,
-) : AnimeFilter.CheckBox(name, state)
+open class TagFilter(val key: String, name: String, state: Boolean = false) :
+    AnimeFilter.CheckBox(name, state)
 
-class GenreFilter(
-    private val translator: ChineseTranslator,
-) : QueryFilter(
-    name = "影片類型",
-    key = "genre",
-    values = arrayOf(
-        "全部",
-        "裏番",
-        "泡面番",
-        "Motion Anime",
-    ),
-) {
-    private val translatedName: String by lazy { runBlocking { translator.translate(name) } }
-    private val translatedValues: Array<String> by lazy {
-        values.map { runBlocking { translator.translate(it) } }.toTypedArray()
-    }
+class GenreFilter(values: Array<String>) :
+    QueryFilter(
+        "Genre",
+        "genre",
+        values.ifEmpty { arrayOf("All", "Hentai", "Short", "Motion Anime") },
+    )
 
-    override fun getName(): String = translatedName
-    override fun getValues(): Array<String> = translatedValues
-}
+class SortFilter(values: Array<String>) :
+    QueryFilter(
+        "Sort By",
+        "sort",
+        values.ifEmpty { arrayOf("Newest Release", "Recently Uploaded", "Top Today", "Top Week", "Top Month") },
+    )
 
-class SortFilter(
-    private val translator: ChineseTranslator,
-) : QueryFilter(
-    name = "排序方式",
-    key = "sort",
-    values = arrayOf(
-        "最新上市",
-        "最新上傳",
-        "本日排行",
-        "本週排行",
-        "本月排行",
-    ),
-) {
-    private val translatedName: String by lazy { runBlocking { translator.translate(name) } }
-    private val translatedValues: Array<String> by lazy {
-        values.map { runBlocking { translator.translate(it) } }.toTypedArray()
-    }
+object HotFilter : TagFilter("sort", "Top Week", true)
 
-    override fun getName(): String = translatedName
-    override fun getValues(): Array<String> = translatedValues
-}
+class YearFilter(values: Array<String>) :
+    QueryFilter("Year", "year", values.ifEmpty { arrayOf("All Years") })
 
-class HotFilter(
-    translator: ChineseTranslator,
-) : TagFilter(
-    key = "sort",
-    name = "本週排行",
-    state = true,
-) {
-    private val translatedName: String by lazy { runBlocking { translator.translate(name) } }
-    override fun getName(): String = translatedName
-}
+class MonthFilter(values: Array<String>) :
+    QueryFilter("Month", "month", values.ifEmpty { arrayOf("All Months") })
 
-class YearFilter(
-    private val translator: ChineseTranslator,
-) : QueryFilter(
-    name = "發佈年份",
-    key = "year",
-    values = arrayOf(
-        "全部年份",
-    ),
-) {
-    private val translatedName: String by lazy { runBlocking { translator.translate(name) } }
-    private val translatedValues: Array<String> by lazy {
-        values.map { runBlocking { translator.translate(it) } }.toTypedArray()
-    }
+class DateFilter(yearFilter: YearFilter, monthFilter: MonthFilter) :
+    AnimeFilter.Group<QueryFilter>("Release Date", listOf(yearFilter, monthFilter))
 
-    override fun getName(): String = translatedName
-    override fun getValues(): Array<String> = translatedValues
-}
+class CategoryFilter(name: String, filters: List<TagFilter>) :
+    AnimeFilter.Group<TagFilter>(name, filters)
 
-class MonthFilter(
-    private val translator: ChineseTranslator,
-) : QueryFilter(
-    name = "發佈月份",
-    key = "month",
-    values = arrayOf(
-        "全部月份",
-    ),
-) {
-    private val translatedName: String by lazy { runBlocking { translator.translate(name) } }
-    private val translatedValues: Array<String> by lazy {
-        values.map { runBlocking { translator.translate(it) } }.toTypedArray()
-    }
+class BroadMatchFilter : TagFilter("broad", "Broad Match")
 
-    override fun getName(): String = translatedName
-    override fun getValues(): Array<String> = translatedValues
-}
-
-class DateFilter(
-    private val translator: ChineseTranslator,
-    yearFilter: YearFilter,
-    monthFilter: MonthFilter,
-) : AnimeFilter.Group<QueryFilter>(
-    name = "發佈日期",
-    filters = listOf(yearFilter, monthFilter),
-) {
-    private val translatedName: String by lazy { runBlocking { translator.translate(name) } }
-    override fun getName(): String = translatedName
-}
+class TagsFilter(filters: List<AnimeFilter<out Any>>) :
+    AnimeFilter.Group<AnimeFilter<out Any>>("Tags", filters)
