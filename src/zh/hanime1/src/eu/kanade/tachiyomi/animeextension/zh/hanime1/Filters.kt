f6323158984b18a296292ccdb1569f8f0d73ffa5
Mohin <122416157+Mohin2295747@users.noreply.github.com>
2025-10-17 23:16:40 +0600

    Update Filters.kt

--- src/zh/hanime1/src/eu/kanade/tachiyomi/animeextension/zh/hanime1/Filters.kt
+++ src/zh/hanime1/src/eu/kanade/tachiyomi/animeextension/zh/hanime1/Filters.kt
@@ -4,38 +4,45 @@ import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 
 open class QueryFilter(name: String, val key: String, values: Array<String>) :
     AnimeFilter.Select<String>(name, values) {
+    
     val selected: String
-        get() = if (state == 0) {
-            ""
-        } else {
-            values[state]
-        }
+        get() = if (state == 0) "" else values[state]
 }
 
 open class TagFilter(val key: String, name: String, state: Boolean = false) :
     AnimeFilter.CheckBox(name, state)
 
-class GenreFilter(values: Array<String>) :
-    QueryFilter(
-        "Video Type",
-        "genre",
-        values.ifEmpty { arrayOf("All", "R-18", "Short Anime", "Motion Anime") },
+class GenreFilter(values: Array<String>) : QueryFilter(
+    "Video Type",
+    "genre",
+    if (values.isNotEmpty()) values else arrayOf("All", "R-18", "Short Anime", "Motion Anime")
+)
+
+class SortFilter(values: Array<String>) : QueryFilter(
+    "Sort By", 
+    "sort",
+    if (values.isNotEmpty()) values else arrayOf(
+        "Latest Release", 
+        "Latest Upload", 
+        "Daily Ranking", 
+        "Weekly Ranking", 
+        "Monthly Ranking"
     )
+)
 
-class SortFilter(values: Array<String>) :
-    QueryFilter(
-        "Sort By",
-        "sort",
-        values.ifEmpty { arrayOf("Latest Release", "Latest Upload", "Daily Ranking", "Weekly Ranking", "Monthly Ranking") },
-    )
+class HotFilter : TagFilter("sort", "Weekly Ranking", true)
 
-object HotFilter : TagFilter("sort", "Weekly Ranking", true)
+class YearFilter(values: Array<String>) : QueryFilter(
+    "Release Year",
+    "year", 
+    if (values.isNotEmpty()) values else arrayOf("All Years")
+)
 
-class YearFilter(values: Array<String>) :
-    QueryFilter("Release Year", "year", values.ifEmpty { arrayOf("All Years") })
-
-class MonthFilter(values: Array<String>) :
-    QueryFilter("Release Month", "month", values.ifEmpty { arrayOf("All Months") })
+class MonthFilter(values: Array<String>) : QueryFilter(
+    "Release Month",
+    "month",
+    if (values.isNotEmpty()) values else arrayOf("All Months")
+)
 
 class DateFilter(yearFilter: YearFilter, monthFilter: MonthFilter) :
     AnimeFilter.Group<QueryFilter>("Release Date", listOf(yearFilter, monthFilter))
@@ -45,5 +52,5 @@ class CategoryFilter(name: String, filters: List<TagFilter>) :
 
 class BroadMatchFilter : TagFilter("broad", "Broad Match")
 
-class TagsFilter(filters: List<AnimeFilter<out Any>>) :
-    AnimeFilter.Group<AnimeFilter<out Any>>("Tags", filters)
+class TagsFilter(filters: List<AnimeFilter<*>>) :
+    AnimeFilter.Group<AnimeFilter<*>>("Tags", filters)
