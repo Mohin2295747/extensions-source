d2bd61e67874611b99b6fc767f14d7d52353adf2
Mohin2295747 <sanowerhossain1982@gmail.com>
2026-01-01 19:58:48 +0600

    ....

--- src/all/missav/src/eu/kanade/tachiyomi/animeextension/all/missav/MissAV.kt
+++ src/all/missav/src/eu/kanade/tachiyomi/animeextension/all/missav/MissAV.kt
@@ -4,7 +4,11 @@ import android.app.Application
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
-import eu.kanade.tachiyomi.animesource.model.*
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.lib.javcoverfetcher.JavCoverFetcher
 import eu.kanade.tachiyomi.lib.javcoverfetcher.JavCoverFetcher.fetchHDCovers
