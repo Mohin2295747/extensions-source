de38289c90bb291c00d485b90f4794cc77159942
Mohin2295747 <sanowerhossain1982@gmail.com>
2026-01-17 21:25:38 +0600

    ....

--- src/zh/hanime1/src/eu/kanade/tachiyomi/animeextension/zh/hanime1/Hanime1.kt
+++ src/zh/hanime1/src/eu/kanade/tachiyomi/animeextension/zh/hanime1/Hanime1.kt
@@ -567,7 +567,7 @@ class Hanime1 : AnimeHttpSource(), ConfigurableAnimeSource {
         val statusHeader = Preference().apply {
             key = "status_header"
             title = "🔍 Connection Status"
-            setSelectable(false)
+            isEnabled = false
         }
         screen.addPreference(statusHeader)
 
@@ -607,7 +607,7 @@ class Hanime1 : AnimeHttpSource(), ConfigurableAnimeSource {
         val cookieHeader = Preference().apply {
             key = "cookie_header"
             title = "🔑 Cookie Management"
-            setSelectable(false)
+            isEnabled = false
         }
         screen.addPreference(cookieHeader)
 
@@ -676,7 +676,7 @@ class Hanime1 : AnimeHttpSource(), ConfigurableAnimeSource {
         val videoHeader = Preference().apply {
             key = "video_header"
             title = "🎥 Video Settings"
-            setSelectable(false)
+            isEnabled = false
         }
         screen.addPreference(videoHeader)
 
@@ -713,7 +713,7 @@ class Hanime1 : AnimeHttpSource(), ConfigurableAnimeSource {
         val helpHeader = Preference().apply {
             key = "help_header"
             title = "❓ Help & Troubleshooting"
-            setSelectable(false)
+            isEnabled = false
         }
         screen.addPreference(helpHeader)
 
